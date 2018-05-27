e32711f6334ef57beabba8cf378995604f325e8d
ang3lino <sigma271@outlook.com>
2018-05-27 17:11:05 -0500

    script funcionando correctamente

--- create_account.php
+++ create_account.php
@@ -1,18 +1,18 @@
 <?php
 
-if ( 
-	isset($_GET['name']) and 
-	isset($_GET['location']) and 
-	isset($_GET['email']) and 
-	isset($_GET['password']) 
+if ( // comprobaciones ambiguas pues en java nos aseguramos que sean no nulas
+	isset($_POST['name']) and 
+	isset($_POST['location']) and 
+	isset($_POST['email']) and 
+	isset($_POST['password']) 
 ) {
-	include "open_connection.php";
+	require_once "open_connection.php";
 	$name = utf8_decode($_POST['name']); // funcion para dar soporte a acentos
 	$location = utf8_decode($_POST['location']);
 	$email = utf8_decode($_POST['email']);
 	$password = utf8_decode($_POST['password']);
-
-	$sql = "select * from votante where email like '%$email%';";
+	// se asume que el email entrante no tiene espacios
+	$sql = "select * from votante where email like '$email';"; 
 	$result = $conn->query($sql);
 	$response =  array();
 
@@ -22,9 +22,10 @@ if (
 		$sql = "INSERT INTO votante(email, nombre, localidad, passwd) values ('$email', '$name', '$location', '$password');";
 		$result = $conn->query($sql);
 		array_push($response, array("success" => 'true'));
+		if (!$result) die('no se pudo');
 	}
 	echo json_encode($response);	
-} else die("no estan llenos todos los campos");
+} else die('no deberia de estar aqui...'); 
 
 $conn->close();
 
