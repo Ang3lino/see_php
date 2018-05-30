9b0fbb3d2b16ab4e5e80ebdb0076ed0a0726b5b3
Angel Lopez Manriquez <sigma271@outlook.com>
2018-05-30 13:41:23 -0500

    pone una imagen nula si el user no la define

--- create_account.php
+++ create_account.php
@@ -11,8 +11,9 @@ if ( // comprobaciones ambiguas pues en java nos aseguramos que sean no nulas
 	$location = utf8_decode($_POST['location']);
 	$email = ($_POST['email']);
 	$password = ($_POST['password']);
-	// $bitmap = $_POST['bitmap'];
-	// $perfil = 'perfil';
+	$bitmap = $_POST['perfil'];
+	$perfil = 'perfil.jpg'; // 
+	$upload_path = "img/$perfil"; // implicitamente $ubicaciondeestescript/img/perfil
 	// se asume que el email entrante no tiene espacios
 	$sql = "select * from votante where email like '$email';"; 
 	$result = $conn->query($sql);
@@ -21,11 +22,14 @@ if ( // comprobaciones ambiguas pues en java nos aseguramos que sean no nulas
 	if ($result->num_rows > 0) {
 		array_push($response, array("success" => 'false'));
 	} else {
-		$sql  = "INSERT INTO votante(email, nombre, localidad, passwd) " .
-				"VALUES ('$email', '$name', '$location', '$password');";
+		$sql  = "INSERT INTO votante(email, nombre, localidad, passwd, perfil) " .
+				"VALUES ('$email', '$name', '$location', '$password', '$perfil');";
 		$result = $conn->query($sql);
 		array_push($response, array("success" => 'true'));
 		if (!$result) die('no se pudo');
+		else {
+			file_put_contents($upload_path, base64_decode($bitmap));
+		}
 	}
 	echo json_encode($response);	
 } else die('no deberia de estar aqui...'); 
