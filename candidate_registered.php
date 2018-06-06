2e31e5542bccdfe843bd20966ef1380e87ba562d
Angel Lopez Manriquez <sigma271@outlook.com>
2018-06-06 12:57:23 -0500

    Add files via upload

--- candidate_registered.php
+++ candidate_registered.php
@@ -1,17 +1,29 @@
 <?php
 
-if (isset($_POST['email'])  {
+if (isset($_POST['email']))  {
 	require_once "open_connection.php";
 	$email = $_POST['email']; 
-	$sql = "select * from votante where email like '$email' and passwd like '$password';"; 
+	$n = $_POST['number'];
+	$sql = "select * from votante where email like '$email';"; 
 	$result = $conn->query($sql);
-	$response =  array();
+	$response = array();
 
-	if ($result->num_rows == 0)  
-		array_push($response, array("exists" => 'false'));
-	else 
-		array_push($response, array("exists" => 'true'));
+	if ($result->num_rows == 1) { // la persona existe
+		$result->data_seek(0); 
+		$row = $result->fetch_array(MYSQLI_ASSOC); 
+		$query = "INSERT INTO postulante(post_email, sala_num) VALUES ('$email', $n); ";
+		$result = $conn->query($query);
+		if ($result)
+			array_push($response, array(
+				'inserted' => 'true',
+				'exists' => 'true',
+				'email' => $row['email'],
+				'name' => $row['nombre']
+			));
+		else array_push($response, array('exists' => 'true', 'inserted' => 'false'));
+	} else  array_push($response, array("exists" => 'false'));
 	echo json_encode($response);	
+
 } else die('exists an empty field'); 
 
 $conn->close();
