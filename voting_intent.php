87e58551c89528f0349190b8e77e1ed0f668e19b
Angel Lopez Manriquez <sigma271@outlook.com>
2018-06-14 23:56:05 -0500

    Add files via upload

--- voting_intent.php
+++ voting_intent.php
@@ -13,33 +13,47 @@ function vote($conn, $n, $email, $post_email) {
 	if (!$result) die("couldn't insert items");
 }
 
+function can_we_vote($conn, $n) {
+	$query = "SELECT se_puede_votar from sala_votacion where numero = {$n}; ";
+	$result = $conn->query($query);
+	if (!$result) die ($conn->error);
+	$result->data_seek(0);
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+	return $row['se_puede_votar'];
+}
+
 if ( isset($_POST['number']) and isset($_POST['email']) and isset($_POST['post_email']) ){
 	require_once "open_connection.php";
 
 	$n = $_POST['number'];
 	$email = $_POST['email'];
 	$post_email = $_POST['post_email'];
-
-	$query = " select v.*  from votante v, medio_votacion m, sala_votacion s 
-				where v.email = m.email and 
-					m.numero = s.numero and 
-					v.email = '$email' and 
-					s.numero = " . $n . " ; ";
-	$result = $conn->query($query); 
-	if (!$result) die ($conn->error); 
-
-	$len = $result->num_rows;
-	$repeated_vote = false;	
-	if ($len == 1) {
-		$repeated_vote = true;
-		delete_vote($conn, $n, $email);
-	}
-
-	vote($conn, $n, $email, $post_email);
 	$response = array();
+	if (can_we_vote($conn, $n)) {
+		$query = " select v.*  from votante v, medio_votacion m, sala_votacion s 
+					where v.email = m.email and 
+						m.numero = s.numero and 
+						v.email = '$email' and 
+						s.numero = " . $n . " ; ";
+		$result = $conn->query($query); 
+		if (!$result) die ($conn->error); 
 
-	array_push($response, array("changed" => $repeated_vote));
-	
+		$len = $result->num_rows;
+		$repeated_vote = false;	
+		if ($len == 1) {
+			$repeated_vote = true;
+			delete_vote($conn, $n, $email);
+		}
+
+		vote($conn, $n, $email, $post_email);
+
+		array_push($response, 
+			array(
+				"can_we_vote" => true,
+				"changed" => $repeated_vote
+			));
+	} else array_push($response, array("can_we_vote" => false));
+
 	echo json_encode($response);	
 	$conn->close();
 
